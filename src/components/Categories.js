d040aeb2c7f27cc10ffa32a3f5da3636049496cb
holybobrius <ilya.povaroff@gmail.com>
2021-02-04 01:29:11 +0300

    Forms are now required to fill

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -10,21 +10,21 @@ function General() {
                 <div className="full-name-div">
                     <label for="full-name">Full Name</label>
                     <br></br>
-                    <input id="full-name" className="info-input" type="text" placeholder="Your Full Name"></input>
+                    <input id="full-name" className="info-input" type="text" placeholder="Your Full Name" required></input>
                     <PElement></PElement>
                 </div>
 
                 <div className="email-div">
                     <label for="email">E-mail</label>
                     <br></br>
-                    <input id="email" className="info-input" type="email" placeholder="Your E-Mail"></input>
+                    <input id="email" className="info-input" type="email" placeholder="Your E-Mail" required></input>
                     <PElement></PElement>
                 </div>
 
                 <div className="phone-number-div">
                     <label for="phone-number">Phone Number</label>
                     <br></br>
-                    <input id="phone-number" className="info-input" type="text" placeholder="Your Phone Number"></input>
+                    <input id="phone-number" className="info-input" type="text" placeholder="Your Phone Number" required></input>
                     <PElement></PElement>
                 </div>
 
@@ -42,25 +42,25 @@ function Education() {
                     <div className="input-div">
                         <label htmlFor="institution">School/College</label>
                         <br></br>
-                        <input className="info-input" id="institution" type="text" placeholder="Your Educational Institution" />
+                        <input className="info-input" id="institution" type="text" placeholder="Your Educational Institution" required/>
                         <PElement></PElement>
                     </div>
                     <div className="input-div">
                         <label htmlFor="degree">Degree</label>
                         <br></br>
-                        <input className="info-input" id="degree" type="text" placeholder="Your Degree/Field of study" />
+                        <input className="info-input" id="degree" type="text" placeholder="Your Degree/Field of study" required />
                         <PElement></PElement>
                     </div>
                     <div className="input-div">
                         <label htmlFor="from">From</label>
                         <br></br>
-                        <input className="info-input" id="from" type="date" />
+                        <input className="info-input" id="from" type="date" required />
                         <PElement></PElement>
                     </div>
                     <div className="input-div">
                         <label htmlFor="to">To</label>
                         <br></br>
-                        <input className="info-input" id="to" type="date" />
+                        <input className="info-input" id="to" type="date" required />
                         <PElement></PElement>
                     </div>
                     <button type="submit">Submit</button>
@@ -77,25 +77,25 @@ function Experience() {
                     <div className="input-div">
                         <label htmlFor="institution">Company Name</label>
                         <br></br>
-                        <input className="info-input" id="institution" type="text" placeholder="Your Company Name" />
+                        <input className="info-input" id="institution" type="text" placeholder="Your Company Name" required />
                         <PElement></PElement>
                     </div>
                     <div className="input-div">
                         <label htmlFor="degree">Position Title</label>
                         <br></br>
-                        <input className="info-input" id="degree" type="text" placeholder="Your Position Title" />
+                        <input className="info-input" id="degree" type="text" placeholder="Your Position Title" required />
                         <PElement></PElement>
                     </div>
                     <div className="input-div">
                         <label htmlFor="from">From</label>
                         <br></br>
-                        <input className="info-input" id="from" type="date" />
+                        <input className="info-input" id="from" type="date" required />
                         <PElement></PElement>
                     </div>
                     <div className="input-div">
                         <label htmlFor="to">To</label>
                         <br></br>
-                        <input className="info-input" id="to" type="date" />
+                        <input className="info-input" id="to" type="date" required />
                         <PElement></PElement>
                     </div>
                     <button type="submit">Submit</button>
