e68634618f297cc42644389194e3ca9095b4684b
holybobrius <ilya.povaroff@gmail.com>
2021-02-04 00:56:40 +0300

    Added PElement.js

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PElement from "./PElement"
 import "../index.css"
 
 function General() {
@@ -10,18 +11,21 @@ function General() {
                     <label for="full-name">Full Name</label>
                     <br></br>
                     <input id="full-name" className="info-input" type="text" placeholder="Your Full Name"></input>
+                    <PElement></PElement>
                 </div>
 
                 <div className="email-div">
-                    <label for="full-name">E-mail</label>
+                    <label for="email">E-mail</label>
                     <br></br>
-                    <input id="full-name" className="info-input" type="email" placeholder="Your E-Mail"></input>
+                    <input id="email" className="info-input" type="email" placeholder="Your E-Mail"></input>
+                    <PElement></PElement>
                 </div>
 
                 <div className="phone-number-div">
-                    <label for="full-name">Phone Number</label>
+                    <label for="phone-number">Phone Number</label>
                     <br></br>
-                    <input id="full-name" className="info-input" type="text" placeholder="Your Phone Number"></input>
+                    <input id="phone-number" className="info-input" type="text" placeholder="Your Phone Number"></input>
+                    <PElement></PElement>
                 </div>
 
                 <button type="submit">Submit</button>
@@ -39,21 +43,25 @@ function Education() {
                         <label htmlFor="institution">School/College</label>
                         <br></br>
                         <input className="info-input" id="institution" type="text" placeholder="Your Educational Institution" />
+                        <PElement></PElement>
                     </div>
                     <div className="input-div">
                         <label htmlFor="degree">Degree</label>
                         <br></br>
                         <input className="info-input" id="degree" type="text" placeholder="Your Degree/Field of study" />
+                        <PElement></PElement>
                     </div>
                     <div className="input-div">
                         <label htmlFor="from">From</label>
                         <br></br>
                         <input className="info-input" id="from" type="date" />
+                        <PElement></PElement>
                     </div>
                     <div className="input-div">
                         <label htmlFor="to">To</label>
                         <br></br>
                         <input className="info-input" id="to" type="date" />
+                        <PElement></PElement>
                     </div>
                     <button type="submit">Submit</button>
                 </form>
@@ -70,21 +78,25 @@ function Experience() {
                         <label htmlFor="institution">Company Name</label>
                         <br></br>
                         <input className="info-input" id="institution" type="text" placeholder="Your Company Name" />
+                        <PElement></PElement>
                     </div>
                     <div className="input-div">
                         <label htmlFor="degree">Position Title</label>
                         <br></br>
                         <input className="info-input" id="degree" type="text" placeholder="Your Position Title" />
+                        <PElement></PElement>
                     </div>
                     <div className="input-div">
                         <label htmlFor="from">From</label>
                         <br></br>
                         <input className="info-input" id="from" type="date" />
+                        <PElement></PElement>
                     </div>
                     <div className="input-div">
                         <label htmlFor="to">To</label>
                         <br></br>
                         <input className="info-input" id="to" type="date" />
+                        <PElement></PElement>
                     </div>
                     <button type="submit">Submit</button>
                 </form>
---
+++ src/components/PElement.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../index.css";
+
+function PElement() {
+    return(
+        <p>test</p>
+    )
+}
+
+export default PElement;
